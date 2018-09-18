71371c6731badb15f997fffefa7041cacb5eaa99
cb <cb@cbcorp.com>
2018-09-18 15:29:54 +0200

    first template sans css modules

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home/home'
 
+import Layout from './hoc/layout/layout'
+
 class Routes extends Component {
   render () {
     return (
-      <Switch>
-        <Route path='/' exact component={Home} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Layout>
     )
   }
 }
